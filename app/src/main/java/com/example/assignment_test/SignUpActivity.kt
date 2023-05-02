019564879479872618b6b2bebc7fca49bbff2d71
Zhi Hin <zhinfoong@gmail.com>
2023-05-02 22:16:27 +0800

    first display page -add background video -start button -text link

--- app/src/main/java/com/example/assignment_test/SignUpActivity.kt
+++ app/src/main/java/com/example/assignment_test/SignUpActivity.kt
@@ -1,27 +1,49 @@
 package com.example.assignment_test
 
 import android.content.Intent
+import android.media.MediaPlayer
 import android.net.Uri
 import android.os.Bundle
+import android.view.Surface
+import android.view.TextureView
+import android.view.View
+import android.view.WindowManager
 import android.widget.Button
+import android.widget.MediaController
+import android.widget.RelativeLayout
+import android.widget.TextView
 import android.widget.VideoView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class SignUpActivity : AppCompatActivity() {
 
-    private lateinit var videoView: VideoView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
 
-        videoView = findViewById(R.id.videoView)
-        val videoUri = Uri.parse("android.resource://" + packageName + "/" + R.raw.start_video)
+        //background video display
+        val videoView = findViewById<VideoView>(R.id.videoView)
+        val videoUri = Uri.parse("android.resource://" + packageName + "/" + R.raw.video)
         videoView.setVideoURI(videoUri)
-        videoView.start()
+        // loop mute and start video
+        videoView.setOnPreparedListener { mp ->
+            mp.isLooping = true
+            mp.setVolume(0f,0f)
+            mp.start()
+        }
 
+        //set up button on click
         val signupButton : Button = findViewById(R.id.signUp_button)
         signupButton.setOnClickListener {
             val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        //set text intent to login activity
+        val goLoginText : TextView = findViewById(R.id.GoToLogin)
+        goLoginText.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
     }
