66b450ca5ef7fc999029987b7a0ad3cd10f4144e
Zhi Hin <zhinfoong@gmail.com>
2023-05-02 16:21:03 +0800

    Sign Up page - add intent function

--- app/src/main/java/com/example/assignment_test/LoginActivity.kt
+++ app/src/main/java/com/example/assignment_test/LoginActivity.kt
@@ -12,7 +12,7 @@ class LoginActivity : AppCompatActivity() {
 
         val loginButton: Button =  findViewById(R.id.signUp_button)
         loginButton.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
+            val intent = Intent(this, HomePageActivity::class.java)
             startActivity(intent)
         }
     }
--- app/src/main/java/com/example/assignment_test/SignUpActivity.kt
+++ app/src/main/java/com/example/assignment_test/SignUpActivity.kt
@@ -1,11 +1,19 @@
 package com.example.assignment_test
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        val signupButton : Button = findViewById(R.id.signUp_button)
+        signupButton.setOnClickListener {
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
