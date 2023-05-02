fc593c29a3600a060a76c3ace5602c08d8aeac33
Zhi Hin <zhinfoong@gmail.com>
2023-05-02 17:29:40 +0800

    sign up page - hvt done add background video

--- app/src/main/java/com/example/assignment_test/SignUpActivity.kt
+++ app/src/main/java/com/example/assignment_test/SignUpActivity.kt
@@ -1,14 +1,23 @@
 package com.example.assignment_test
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
+import android.widget.VideoView
 import androidx.appcompat.app.AppCompatActivity
 
 class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var videoView: VideoView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        videoView = findViewById(R.id.videoView)
+        val videoUri = Uri.parse("android.resource://" + packageName + "/" + R.raw.start_video)
+        videoView.setVideoURI(videoUri)
+        videoView.start()
 
         val signupButton : Button = findViewById(R.id.signUp_button)
         signupButton.setOnClickListener {
