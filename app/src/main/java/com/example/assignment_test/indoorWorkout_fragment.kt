ff84500df5c2385b6bd100b4f8bb719ee34a6328
Dickie Wong <dickiewwh23@gmail.com>
2023-05-02 14:56:22 +0800

    Added new files for HomePage modules

---
+++ app/src/main/java/com/example/assignment_test/indoorWorkout_fragment.kt
@@ -0,0 +1,41 @@
+package com.example.assignment_test
+
+import android.os.Bundle
+import android.text.TextUtils.replace
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
+
+
+class indoorWorkout_fragment : Fragment(R.layout.fragment_indoor_workout_fragment) {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_indoor_workout_fragment, container, false)
+
+        val indoorLvl1: ImageButton = view.findViewById(R.id.imageButton1)
+        val indoorLvl2: ImageButton = view.findViewById(R.id.imageButton2)
+        // Set onClickListener for the button here
+
+
+
+        indoorLvl1.setOnClickListener{
+            println("IMAGE ONE IS CLICKED")
+
+        }
+
+        indoorLvl2.setOnClickListener{
+            println("IMAGE TWO IS CLICKED")
+
+        }
+        return view
+    }
+
+    }
+
+
+
---
+++ app/src/main/java/com/example/assignment_test/outdoorWorkout_fragment.kt
@@ -0,0 +1,18 @@
+package com.example.assignment_test
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+
+
+class outdoorWorkout_fragment : Fragment(R.layout.fragment_outdoor_workout_fragment) {
+
+
+
+
+
+}
