e498d36df8f31bc17810a777ba31e1551796c058
Dickie Wong <dickiewwh23@gmail.com>
2023-05-04 19:08:09 +0800

    -refactored icon(naming) in drawable -seperated homePage & workout activity

--- app/src/main/java/com/example/assignment_test/HomePageActivity.kt
+++ app/src/main/java/com/example/assignment_test/HomePageActivity.kt
@@ -2,38 +2,10 @@ package com.example.assignment_test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.TextUtils.replace
-import android.widget.Button
-import android.widget.ImageButton
 
 class HomePageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
-
-        val indoorworkoutFragment = indoorWorkout_fragment()
-        val outdoorworkoutFragment = outdoorWorkout_fragment()
-        val indoorBtn: Button =  findViewById(R.id.indoor)
-        val outdoorBtn: Button =  findViewById(R.id.outdoor)
-        supportFragmentManager.beginTransaction().apply {
-            replace(R.id.workout, indoorworkoutFragment)
-            commit()
-        }
-
-        indoorBtn.setOnClickListener{
-            supportFragmentManager.beginTransaction().apply {
-                replace(R.id.workout, indoorworkoutFragment)
-                addToBackStack(null)
-                commit()
-            }
-        }
-
-        outdoorBtn.setOnClickListener{
-            supportFragmentManager.beginTransaction().apply {
-                replace(R.id.workout, outdoorworkoutFragment)
-                addToBackStack(null)
-                commit()
-            }
-        }
     }
 }
