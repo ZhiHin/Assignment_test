cc5f3b2d3877ce68db02ea26acade7d404bb3090
Dickie Wong <dickiewwh23@gmail.com>
2023-05-04 21:51:32 +0800

    Fixed bottom navigation bar (selected page is now functional)

--- app/src/main/java/com/example/assignment_test/HomePageActivity.kt
+++ app/src/main/java/com/example/assignment_test/HomePageActivity.kt
@@ -11,6 +11,7 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.CompositePageTransformer
 import androidx.viewpager2.widget.MarginPageTransformer
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlin.math.abs
 
 class HomePageActivity : AppCompatActivity() {
@@ -20,13 +21,18 @@ class HomePageActivity : AppCompatActivity() {
     private lateinit var imageList:ArrayList<Int>
     private lateinit var adapter: ImageSlider
 
+    private lateinit var bottomNavigationView: BottomNavigationView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home_page)
 
+        setContentView(R.layout.activity_home_page)
         init()
         setUpTransformer()
+
+
+        setUpBottomNavigationView()
 
         viewPager2.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
             override fun onPageSelected(position: Int) {
@@ -83,7 +89,40 @@ class HomePageActivity : AppCompatActivity() {
         viewPager2.clipChildren = false
         viewPager2.getChildAt(0).overScrollMode = RecyclerView.OVER_SCROLL_NEVER
 
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+        bottomNavigationView.selectedItemId = R.id.home
     }
+    public fun setUpBottomNavigationView() {
+
+        bottomNavigationView.setOnNavigationItemSelectedListener { item: MenuItem ->
+            when (item.itemId) {
+                R.id.workout -> {
+                    // navigate to home activity
+                    val intent = Intent(this, WorkOutPageActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.report -> {
+                    // navigate to profile activity
+                    val intent = Intent(this, WorkOutPageActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.home -> {
+                    // navigate to settings activity
+                    val intent = Intent(this, HomePageActivity::class.java)
+                    startActivity(intent)
+                    true
+
+
+                }
+                else -> false
+            }
+        }
+
+
+    }
+
 
 
 }
