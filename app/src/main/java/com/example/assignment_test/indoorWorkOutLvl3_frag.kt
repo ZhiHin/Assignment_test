4c249fad7de22a3c3d42c94b12d0729f13f194d6
Dickie Wong <dickiewwh23@gmail.com>
2023-05-14 18:05:22 +0800

    -fixed aliignment problem\ -added indoorworkoutLvl 2 & 3

--- app/src/main/java/com/example/assignment_test/indoorWorkOutLvl3_frag.kt
+++ app/src/main/java/com/example/assignment_test/indoorWorkOutLvl3_frag.kt
@@ -1,55 +1,187 @@
 package com.example.assignment_test
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.text.TextUtils.replace
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import android.view.Window
+import android.view.WindowManager
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import pl.droidsonroids.gif.GifImageView
+import java.lang.Math.ceil
 
-/**
- * A simple [Fragment] subclass.
- * Use the [indoorWorkOutLvl3_frag.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class indoorWorkOutLvl3_frag : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var currentExercisePosition = 0
+    private lateinit var currentExercise: Exercise
+    private lateinit var timer: CountDownTimer
+    private lateinit var progressBar: ProgressBar
+    private var timerStarted = false
+    private lateinit var nameTextView: TextView
+    private lateinit var gifImageView: GifImageView
+    private var window: Window? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_indoor_work_out_lvl1_frag, container, false)
+
+        nameTextView = view.findViewById<TextView>(R.id.nameWorkout)
+        gifImageView = view.findViewById<GifImageView>(R.id.gifImageView)
+        val timerTextView = view.findViewById<TextView>(R.id.timerDuration)
+        val nextButton = view.findViewById<AppCompatImageView>(R.id.nextWorkout)
+        val previousButton = view.findViewById<AppCompatImageView>(R.id.previousWorkout)
+        val pauseButton = view.findViewById<AppCompatImageView>(R.id.pauseWorkout)
+        progressBar = view.findViewById<ProgressBar>(R.id.progress_bar)
+        progressBar.progress = 0
+        val window = requireActivity()!!.window
+        window!!.setBackgroundDrawableResource(android.R.color.transparent)
+
+        currentExercise = exercises[currentExercisePosition]
+
+        nameTextView.text = currentExercise.name
+        gifImageView.setImageResource(currentExercise.gifResourceId)
+
+        startTimer(timerTextView)
+
+        nextButton.setOnClickListener {
+
+
+            timer.cancel()
+            currentExercisePosition++
+            if (currentExercisePosition >= exercises.size) {
+                // Last exercise completed
+                timerStarted = false
+                progressBar.progress = 100 // Set progress to 100% to indicate completion
+                nameTextView.text = "Completed" // Set text to "Complete"
+                timerTextView.text = "Nice !"
+                // Set image to check mark icon
+                // gifImageView.setImageResource(R.drawable.check_mark)
+            } else {
+                currentExercise = exercises[currentExercisePosition]
+                nameTextView.text = currentExercise.name
+                gifImageView.setImageResource(currentExercise.gifResourceId)
+                timerTextView.text = "${currentExercise.duration} seconds"
+                timerStarted = false
+                startTimer(timerTextView)
+            }
+
+        }
+
+
+        previousButton.setOnClickListener {
+
+            if (currentExercisePosition == 0) {
+                timer.cancel()
+                timerStarted = false
+                progressBar.progress = 0
+                currentExercise = exercises[currentExercisePosition]
+                nameTextView.text = currentExercise.name
+                gifImageView.setImageResource(currentExercise.gifResourceId)
+                timerTextView.text = "${currentExercise.duration} seconds"
+                startTimer(timerTextView)
+                Toast.makeText(requireContext(), "You're already at the first exercise!", Toast.LENGTH_SHORT).show()
+            } else {
+                timer.cancel()
+                currentExercisePosition--
+                currentExercise = exercises[currentExercisePosition]
+                nameTextView.text = currentExercise.name
+                gifImageView.setImageResource(currentExercise.gifResourceId)
+                timerTextView.text = "${currentExercise.duration} seconds"
+                timerStarted = false
+                startTimer(timerTextView)
+            }
+        }
+
+
+        pauseButton.setOnClickListener {
+            if (timerStarted) {
+                // pause the timer
+                timer.cancel()
+                timerStarted = false
+
+                nextButton.isEnabled = false
+                previousButton.isEnabled = false
+
+                // change the button icon to play button
+                pauseButton.setImageResource(R.drawable.logo_play)
+            } else {
+                // start the timer
+                startTimer(timerTextView)
+                timerStarted = true
+
+                nextButton.isEnabled = true
+                previousButton.isEnabled = true
+
+                // change the button icon to pause button
+                pauseButton.setImageResource(R.drawable.logo_stop_circle)
+            }
+        }
+        return view
+    }
+
+    private fun startTimer(timerTextView: TextView) {
+        timer = object : CountDownTimer((currentExercise.duration * 1000).toLong(), 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                val remainingSeconds = ceil(millisUntilFinished / 1000.0).toInt()
+                timerTextView.text = "$remainingSeconds seconds"
+                val progress = ((currentExercise.duration - remainingSeconds) * 100) / currentExercise.duration
+                progressBar.progress = progress
+            }
+
+            override fun onFinish() {
+                currentExercisePosition++
+                if (currentExercisePosition >= exercises.size) {
+                    // Last exercise completed
+                    timerStarted = false
+                    progressBar.progress = 100 // Set progress to 100% to indicate completion
+                    nameTextView.text = "Completed" // Set text to "Complete"
+                    timerTextView.text = "Nice !"
+//                    gifImageView.setImageResource(R.drawable.) // Set image to check mark icon
+                    return // Stop looping
+                }
+                currentExercise = exercises[currentExercisePosition]
+                progressBar.progress = 0
+                nameTextView.text = currentExercise.name
+                gifImageView.setImageResource(currentExercise.gifResourceId)
+                timerStarted = false
+                timerTextView.text = "${currentExercise.duration} seconds"
+                startTimer(timerTextView)
+            }
+        }
+
+        if (!timerStarted) {
+            timerStarted = true
+            timer.start()
         }
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_indoor_work_out_lvl3_frag, container, false)
+    companion object {
+        val exercises = arrayOf(
+            Exercise("Bent Over Twists", R.drawable.gif_bent_over_twist, 45),
+            Exercise("Jumping Jacks", R.drawable.gif_jumping_jacks, 50),
+            Exercise("Burpees", R.drawable.gif_burpees, 120),
+            Exercise("Squat", R.drawable.gif_squat, 60),
+            Exercise("Back Extension", R.drawable.gif_back_extension, 30),
+            Exercise("Run in Place", R.drawable.gif_run_in_place, 120),
+            Exercise("Reverse Crunches", R.drawable.gif_reverse_crunches, 60),
+            Exercise("Arm Circles", R.drawable.gif_arm_circles, 30),
+            Exercise("Standing Criss-Cross", R.drawable.gif_standing_criss_cross, 50),
+            Exercise("Plank", R.drawable.gif_plank, 120)
+        )
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment indoorWorkOutLvl3_frag.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic fun newInstance(param1: String, param2: String) =
-                indoorWorkOutLvl3_frag().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
-                }
-    }
+    data class Exercise(val name: String, val gifResourceId: Int, val duration: Int)
 }
